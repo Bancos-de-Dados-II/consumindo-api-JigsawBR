32e44ccd23cc3ad1d559aa101d5d244bdb534069
Jigsaw <matheus_jigsaw@outlook.com>
2025-05-08 13:39:49 -0300

    Criando o arquivo script.js.

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const btSalvar = document.getElementById("btSalvar");
+
+btSalvar.addEventListener('click', function(){
+    const titulo = document.getElementById("titulo");
+    const descricao = document.getElementById("descricao");
+    const tipo = document.getElementById("tipo");
+    
+    const task = {
+        titulo: titulo.value,
+        descricao: descricao.value,
+        tipo: tipo.value
+    };
+
+    try {
+        fetch("http://localhost:3000/tasks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(task)
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error("Erro na resposta da API");
+            }
+            return response.json();
+        }).then(data => {
+            console.log("Task criada com sucesso:", data);
+        });
+    } catch(err) {
+        console.log("Erro ao criar task: " + err);
+    }
+});
+
