b5b8cfba688bf0928b633cc8f115d25826fddee0
JigsawBR <matheus_jigsaw@outlook.com>
2025-05-23 13:30:23 -0300

    Adicionando SUPABASE ao Sequelize.

--- database/sequelize.js
+++ database/sequelize.js
@@ -2,10 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres'
-});
+const sequelize = new Sequelize(process.env.SUPABASE_URL);
 
 conectar();
 
